5e44eee6cce18fbaaabb9a60d1ae931e022a7f6a
Udhav Varma <udhavvarma03@gmail.com>
2023-09-05 20:18:13 +0530

    testcase 1 3 fail

--- Part3/mylib.c
+++ Part3/mylib.c
@@ -12,9 +12,10 @@ void *memalloc(unsigned long size)
 	printf("memalloc() called\n");
 	size += 8;
 	size = 8*((size + 7)/8);
+	if(size < 24) size = 24;
 	unsigned long * ptr = head;
 	while(ptr != NULL){
-		unsigned long * freespace = *(ptr);
+		unsigned long freespace = *(ptr);
 		if(freespace <= size){
 			break;
 		}
@@ -48,12 +49,12 @@ void *memalloc(unsigned long size)
 		*(ptr) = size;
 		void * returnPointer = (void*) (ptr + 1);
 		if(nextFree != NULL){
-			*(nextFree + 2) = prevFree;
+			*(nextFree + 2) = (unsigned long) prevFree;
 		}
 		if(prevFree != NULL){
-			*(prevFree + 1) = nextFree;
+			*(prevFree + 1) = (unsigned long) nextFree;
 		}
-		if(nextFree - size >= 24){
+		if(spaceAvailable - size >= 24){
 			unsigned long* endPointer = ptr + (size/8);
 			*endPointer = spaceAvailable - size;
 			*(endPointer + 2) = 0;
@@ -67,9 +68,60 @@ void *memalloc(unsigned long size)
 	return NULL;
 }
 
-int memfree(void *ptr)
+int memfree(void *ptr_)
 {
 	printf("memfree() called\n");
-
+	unsigned long * ptr = (unsigned long*) ptr;
+	ptr -= 1;
+	unsigned long size = *ptr;
+	unsigned long * iterptr = head;
+	while(iterptr != NULL){
+		unsigned long sz = *iterptr;
+		if(iterptr + (sz/8) == ptr){	// Coalesce with previous block (beginning from iterptr), remove from linked list
+			size += sz;
+			unsigned long * prev = (unsigned long*) (iterptr + 2);
+			unsigned long * next = (unsigned long*) (iterptr + 1);
+			if(next != NULL){
+				*(next + 2) = (unsigned long) prev;
+			}
+			if(prev != NULL){
+				*(prev + 1) = (unsigned long) next;
+			}
+			ptr = iterptr;
+			iterptr = next;
+		}
+		else if(ptr + (size/8) == iterptr){	// Coalesce ptr with the next block from iterptr, remove from linked list
+			size += sz;
+			unsigned long * prev = (unsigned long*) (iterptr + 2);
+			unsigned long * next = (unsigned long*) (iterptr + 1);
+			if(next != NULL){
+				*(next + 2) = (unsigned long) prev;
+			}
+			if(prev != NULL){
+				*(prev + 1) = (unsigned long) next;
+			}
+			iterptr = next;
+		}
+		else{
+			iterptr = (unsigned long*) *(iterptr + 1); 
+		}
+	}
+	*ptr = size;
+	*(ptr + 1) = (unsigned long) head;
+	*(ptr + 2) = 0;
+	if(head != NULL)
+		*(head + 2) = (unsigned long) ptr;
+	head = ptr;
 	return 0;
 }	
+
+
+int main(void)
+{
+	char *p = 0;
+	char *q = 0;
+	p = (char*) memalloc(1);
+	q = (char*) memalloc(9);
+	printf("%p %p\n", p, q);
+	return 0;	
+}
