1d36d8290514388de933c0727527b49fdf10eca9
Udhav Varma <udhavvarma03@gmail.com>
2023-09-05 18:20:37 +0530

    Done memalloc, untested

--- Part3/mylib.c
+++ Part3/mylib.c
@@ -13,6 +13,7 @@ void *memalloc(unsigned long size)
 	size += 8;
 	size = 8*((size + 7)/8);
 	unsigned long * ptr = head;
+	unsigned long * prev = NULL;
 	while(ptr != NULL){
 		unsigned long * freespace = *(ptr);
 		if(freespace <= size){
@@ -20,10 +21,54 @@ void *memalloc(unsigned long size)
 		}
 		else{
 			unsigned long * nextaddr = (unsigned long*)(*(ptr + 1));
+			prev = ptr;
 			ptr = nextaddr;
 		}
 	}
-	
+	if(ptr == NULL){	// Not enough space
+		unsigned long sizeofNewBlock = memblock*((size + memblock - 1)/memblock);	// Ceiling of size/memblock
+		unsigned long * newBlockaddr = (unsigned long*) mmap(NULL, sizeofNewBlock, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+		if(sizeofNewBlock < size + 24) size = sizeofNewBlock;	// Padding (if necessary)
+		*newBlockaddr = size;
+		void * returnPointer = (void*) (newBlockaddr + 1);
+		if(sizeofNewBlock - size >= 24){
+			unsigned long * endPointer = newBlockaddr + (size/8);
+			*endPointer = sizeofNewBlock - size;
+			*(endPointer + 1) = 0;
+			*(endPointer + 2) = (unsigned long) prev;
+			if(prev != NULL)
+				*(prev + 1) = (unsigned long) endPointer;
+		}
+		return returnPointer;
+	}	
+	else{	// Enough space at ptr block
+		unsigned long spaceAvailable = *(ptr);
+		unsigned long * nextFree = (unsigned long*) *(ptr + 1);
+		if(spaceAvailable < size + 24) size = spaceAvailable;	// Padding
+		*(ptr) = size;
+		void * returnPointer = (void*) (ptr + 1);
+		if(nextFree - size >= 24){
+			unsigned long* endPointer = ptr + (size/8);
+			*endPointer = spaceAvailable - size;
+			*(endPointer + 1) = (unsigned long) nextFree;
+			*(endPointer + 2) = (unsigned long) prev;
+			if(prev != NULL){
+				*(prev + 1) = (unsigned long) endPointer;
+			}
+			if(nextFree != NULL){
+				*(nextFree + 2) = (unsigned long) endPointer;
+			}
+		}
+		else{
+			if(nextFree != NULL){
+				*(nextFree + 2) = (unsigned long) prev;
+			}
+			if(prev != NULL){
+				*(prev + 1) = (unsigned long) nextFree;
+			}
+		}
+		return returnPointer;
+	}
 	return NULL;
 }
 
