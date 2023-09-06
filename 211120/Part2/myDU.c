35eacc6ccbb932c1b11cba7aacfdb51388c5228c
Udhav Varma <udhavvarma03@gmail.com>
2023-09-06 15:55:56 +0530

    working on Part 2

--- 211120/Part2/myDU.c
+++ 211120/Part2/myDU.c
@@ -5,6 +5,58 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+
+unsigned long findsize(char* path)
+{
+	unsigned long ans = 4096;
+	struct stat buf;
+	DIR * fobj = opendir(path);
+	struct dirent * obj = readdir(fobj);
+	while(obj != NULL){
+		if(strcmp(obj->d_name, "..") == 0 || strcmp(obj->d_name, ".") == 0){
+			obj = readdir(fobj);
+			continue;
+		}
+		if(obj->d_type == DT_DIR){
+			char cfile[5000];
+			strcpy(cfile, path);
+			strcat(cfile, "/");
+			strcat(cfile, obj->d_name);
+			ans += findsize(cfile);
+		}
+		else if(obj->d_type == DT_REG){
+			char nbuf[5000];
+			strcpy(nbuf, path);
+			strcat(nbuf, "/");
+			strcat(nbuf, obj->d_name);
+			int code = stat(nbuf, &buf);
+			if(code == -1){
+				perror("stat at line 33 failed\n");
+				exit(EXIT_FAILURE);
+			}
+			ans += buf.st_size;
+		}
+		else if(obj->d_type == DT_LNK){
+			char nbuf[5000];
+			strcpy(nbuf, path);
+			strcat(nbuf, "/");
+			strcat(nbuf, obj->d_name);
+			int code = stat(nbuf, &buf);
+			if(code == -1){
+				perror("stat at line 44 failed\n");
+				exit(EXIT_FAILURE);
+			}
+			if(S_ISREG(buf.st_mode)){
+				ans += buf.st_size;
+			}	
+			else{
+				ans += findsize(nbuf);
+			}
+		}
+		obj = readdir(fobj);
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	if(argc < 2){
@@ -30,36 +82,34 @@ int main(int argc, char* argv[])
 			strcpy(cfile, originalName);
 			strcat(cfile, "/");
 			strcat(cfile, obj->d_name);
-			// fprintf(stderr, "Folder: %s\n", cfile);
-			int filed[2];
-			int code = pipe(filed);
-			if(code == -1){
-				perror("Pipe fail\n");
-				exit(EXIT_FAILURE);
-			}
-			int pid = fork();
-			if(pid < 0){
-				perror("Fork failed\n");
-				exit(EXIT_FAILURE);
-			}
-			if(pid == 0){
-				dup2(filed[1], STDOUT_FILENO);
-				execl("./myDU", "myDU", cfile, NULL);	
-			}
-			else{
-				char filesize[15];
-				read(filed[0], filesize, 12);
-				ans += atoll(filesize);
-			}
+			ans += findsize(cfile);
+			// // fprintf(stderr, "Folder: %s\n", cfile);
+			// int filed[2];
+			// int code = pipe(filed);
+			// if(code == -1){
+			// 	perror("Pipe fail\n");
+			// 	exit(EXIT_FAILURE);
+			// }
+			// int pid = fork();
+			// if(pid < 0){
+			// 	perror("Fork failed\n");
+			// 	exit(EXIT_FAILURE);
+			// }
+			// if(pid == 0){
+			// 	dup2(filed[1], STDOUT_FILENO);
+			// 	execl("./myDU", "myDU", cfile, NULL);	
+			// }
+			// else{
+			// 	char filesize[15];
+			// 	read(filed[0], filesize, 12);
+			// 	ans += atoll(filesize);
+			// }
 		}
 		else if(obj->d_type == DT_REG){
 			char nbuf[5000];
 			strcpy(nbuf, filename);
-			int namelen = strlen(obj->d_name);
-			char* relname = (char*) malloc((namelen + 2)*sizeof(char));
-			relname[0] = '/';
-			strcpy(relname + 1, obj->d_name);
-			strcat(nbuf, relname);
+			strcat(nbuf, "/");
+			strcat(nbuf, obj->d_name);
 			int code = stat(nbuf, &buf);
 			if(code == -1){
 				perror("Stat failed\n");
@@ -68,7 +118,22 @@ int main(int argc, char* argv[])
 			// fprintf(stderr, "%s\n", nbuf);
 			// fprintf(stderr, "%d\n", val);
 			ans += buf.st_size;
-			free(relname);
+		}
+		else if(obj->d_type == DT_LNK){
+			char nbuf[5000];
+			strcpy(nbuf, filename);
+			strcat(nbuf, "/");
+			strcat(nbuf, obj->d_name);
+			int code = stat(nbuf, &buf);
+			if(code == -1){
+				exit(EXIT_FAILURE);
+			}
+			if(S_ISREG(buf.st_mode)){
+				ans += buf.st_size;
+			}	
+			else{
+				ans += findsize(nbuf);
+			}
 		}
 		// fprintf(stderr, "%s %d\n", obj->d_name, obj->d_type);
 		obj = readdir(fobj);
